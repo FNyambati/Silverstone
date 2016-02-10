c5fa2e18132f3be22fd43ce34279e5f2f0a85bd1
Fred Nyambati <Fred@Admins-MacBook-Pro-3.local>
2016-02-10 00:32:40 -0700

    finished edit account form and styling

--- public/app/controllers/profileCtrl.js
+++ public/app/controllers/profileCtrl.js
@@ -1,4 +1,12 @@
 angular.module('myApp').controller('profileCtrl', function($scope, user) {
   $scope.user = user;
   console.log($scope);
+  $(document).ready(function() {
+      $('#accountPhone')
+                  .intlTelInput({
+                  utilsScript: '/libraries/utils.js',
+                  autoPlaceholder: true,
+                  preferredCountries: ['us', 'gb', 'ke']
+              });
+  });
 });
